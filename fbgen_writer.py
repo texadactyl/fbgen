e61ed3b7730fc850bafdd9a2f3d391e8ac5fc151
Richard Elkins <richard.elkins@gmail.com>
2020-05-29 11:43:54 -0500

    Support all Filterbank data types

--- fbgen_writer.py
+++ fbgen_writer.py
@@ -1,6 +1,7 @@
 """
 fbgen data writer functions
 """
+from math import floor
 import numpy as np
 from tqdm import tqdm
 from blimpy.io.sigproc import generate_sigproc_header
@@ -18,7 +19,7 @@ def write_header(arg_fbobj, out_path, out_fwobj):
         oops("write_header: write({}) failed, reason: {}"
              .format(out_path, repr(err)))
 
-def populate_freqs_hz(arg_fbobj):
+def generate_freqs_hz(arg_fbobj):
     """
     Based on the channel indices, construct a frequency array
     and return it to caller.  Units are Hz, not MHz.
@@ -32,7 +33,7 @@ def populate_freqs_hz(arg_fbobj):
         first_freq = arg_fbobj.f_end * 1e6
     else:
         first_freq = arg_fbobj.f_begin * 1e6
-    i_vals = np.arange(arg_fbobj.chan_start_idx, arg_fbobj.chan_stop_idx)
+    i_vals = np.arange(0, arg_fbobj.header['nchans'])
     freqs = foff * i_vals + first_freq
     return freqs
 
@@ -54,37 +55,41 @@ def get_noisy(arg_signal, arg_max_noise, arg_rfactor):
 def write_data(arg_fbobj, out_path, out_fwobj):
     """Write the data block for each sample. """
     tsamp = arg_fbobj.header["tsamp"]
-    freqs = populate_freqs_hz(arg_fbobj)
+    freqs = generate_freqs_hz(arg_fbobj)
     len_freqs = len(freqs)
+    n_bytes = floor(arg_fbobj.header['nbits'] / 8)
     logger("write_data: #freqs = {}, #samples = {}, low = {:e}, high = {:e}"
            .format(len(freqs), arg_fbobj.t_end, arg_fbobj.signal_low, arg_fbobj.signal_high))
-    et = 0.0
-    for ndx in tqdm(range(arg_fbobj.t_end), desc="Writing ...", unit=" (channel-blocks)"):
-        signal = get_signal(freqs, et, arg_fbobj.signal_low, arg_fbobj.signal_high)
+    elapsed_time = 0.0
+    for ndx in tqdm(range(arg_fbobj.t_begin, arg_fbobj.t_end),
+                    desc="Writing ...", unit=" (channel-blocks)"):
+        signal = get_signal(freqs, elapsed_time,
+                            arg_fbobj.signal_low, arg_fbobj.signal_high)
         if DEBUGGING:
-            print("\nDEBUG low={}, high={}, signal:\n{}".format(arg_fbobj.signal_low, arg_fbobj.signal_high, signal))
+            print("\nDEBUG low={}, high={}, signal:\n{}"
+                  .format(arg_fbobj.signal_low, arg_fbobj.signal_high, signal))
         rfactor = np.random.uniform(-0.5, 0.5, len_freqs)
         noisy64 = get_noisy(signal, arg_fbobj.max_noise, rfactor)
         if DEBUGGING:
             print("DEBUG noisy64:", noisy64)
-        if arg_fbobj.n_bytes == 4:
+        if n_bytes == 4:
             noisy = np.float32(noisy64)
-        elif arg_fbobj.n_bytes == 2:
+        elif n_bytes == 2:
             noisy = np.int16(noisy64)
-        elif arg_fbobj.n_bytes == 1:
+        elif n_bytes == 1:
             noisy = np.int8(noisy64)
         else:
-            oops("write_data: write({}) block #{} failed, invalid n_bytes: {}"
-                 .format(arg_fbobj.n_bytes))
+            oops("write_data: write({}) block #{} failed, invalid n_bytes parameter: {}"
+                 .format(ndx, out_path, n_bytes))
         try:
             out_fwobj.write(noisy)
         except Exception as err:
             oops("write_data: write({}) block #{} failed, reason: {}"
                  .format(ndx, out_path, repr(err)))
-        et += tsamp
+        elapsed_time += tsamp
     print("\n")
 
 if __name__ == "__main__":
-    freqs = np.array([101e6, 102e6, 103e6, 104e6, 105e6])
-    signal = get_signal(freqs, 4, 8)
-    print("signal:\n", signal)
+    frequencies = np.array([101e6, 102e6, 103e6, 104e6, 105e6])
+    signal_array = get_signal(frequencies, 42, 4, 8)
+    print("signal:\n", signal_array)
